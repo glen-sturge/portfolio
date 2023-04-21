61e32c6c66f747d2fa262c5a673e4a6b1962e508
Glen Sturge <glen.sturge@keyin.com>
2023-04-21 19:49:53 -0230

    script-cleanup

--- scripts/script.js
+++ scripts/script.js
@@ -29,38 +29,71 @@ function changeBG() {
   }
 }
 
-const modal1 = document.querySelector("#modal1");
-const modal2 = document.querySelector("#modal2");
-const modal3 = document.querySelector("#modal3");
-const modal4 = document.querySelector("#modal4");
-const openModal1 = document.querySelector("#open-modal1");
-const openModal2 = document.querySelector("#open-modal2");
-const openModal3 = document.querySelector("#open-modal3");
-const openModal4 = document.querySelector("#open-modal4");
+// const modal1 = document.querySelector("#modal1");
+// const modal2 = document.querySelector("#modal2");
+// const modal3 = document.querySelector("#modal3");
+// const modal4 = document.querySelector("#modal4");
 
-openModal1.addEventListener("click", () => {
-  modal1.showModal();
+// const openModal1 = document.querySelector("#open-modal1");
+// const openModal2 = document.querySelector("#open-modal2");
+// const openModal3 = document.querySelector("#open-modal3");
+// const openModal4 = document.querySelector("#open-modal4");
+
+// openModal1.addEventListener("click", () => {
+//   modal1.showModal();
+// });
+// modal1.addEventListener("click", () => {
+//   modal1.close();
+// });
+// openModal2.addEventListener("click", () => {
+//   modal2.showModal();
+// });
+// modal2.addEventListener("click", () => {
+//   modal2.close();
+// });
+// openModal3.addEventListener("click", () => {
+//   modal3.showModal();
+// });
+// modal3.addEventListener("click", () => {
+//   modal3.close();
+// });
+// openModal4.addEventListener("click", () => {
+//   modal4.showModal();
+// });
+// modal4.addEventListener("click", () => {
+//   modal4.close();
+// });
+
+// const modals = [
+//   document.querySelector("#modal1"),
+//   document.querySelector("#modal2"),
+//   document.querySelector("#modal3"),
+//   document.querySelector("#modal4"),
+// ];
+
+// const openModal = [
+//   document.querySelector("#open-modal1"),
+//   document.querySelector("#open-modal2"),
+//   document.querySelector("#open-modal3"),
+//   document.querySelector("#open-modal4"),
+// ];
+
+// written into classes is definitely most satisfying.
+const modals = document.querySelectorAll(".modal");
+const openModal = document.querySelectorAll(".open-modal");
+console.log(modals);
+console.log(openModal);
+
+openModal.forEach((link, index) => {
+  link.addEventListener("click", () => {
+    modals[index].showModal();
+  });
 });
-modal1.addEventListener("click", () => {
-  modal1.close();
-});
-openModal2.addEventListener("click", () => {
-  modal2.showModal();
-});
-modal2.addEventListener("click", () => {
-  modal2.close();
-});
-openModal3.addEventListener("click", () => {
-  modal3.showModal();
-});
-modal3.addEventListener("click", () => {
-  modal3.close();
-});
-openModal4.addEventListener("click", () => {
-  modal4.showModal();
-});
-modal4.addEventListener("click", () => {
-  modal4.close();
+
+modals.forEach((modal) => {
+  modal.addEventListener("click", () => {
+    modal.close();
+  });
 });
 
 linkedIn = document.getElementById("linkedin");
